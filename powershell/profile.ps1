08429f27524af33c7161079d9a6c347525b104d0
Jared Brogan <42228088+jaredbrogan@users.noreply.github.com>
2021-08-14 15:26:54 -0500

    Add files via upload

---
+++ powershell/profile.ps1
@@ -0,0 +1,12 @@
+$Desktop = [Environment]::GetFolderPath("Desktop")
+Set-Location $Desktop;
+Set-PSReadlineOption -BellStyle None;
+$host.ui.RawUI.WindowTitle="BroBro Command Center";
+#$host.ui.RawUI.BackgroundColor = "Black";
+#$host.ui.RawUI.ForegroundColor = "Red"
+$colors = $host.privatedata
+
+function prompt            
+{            
+    "PS " + $(get-location) + " [$(Get-Date -Format F)]> "            
+}
